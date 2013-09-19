8b6a9518f62e8540d234dbded04d61f24758e0d9
martinSternelius <martin.sternelius@gmail.com>
2013-09-19 19:09:00 +0200

    Adds more fields to the Listing model

--- housingtrader/models.py
+++ housingtrader/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*- 
 from django.db import models
+from django.contrib.localflavor.se.forms import SEPostalCodeField, SECountySelect
 
 def generate_rooms_choices(max_rooms_choice):
     '''
@@ -31,15 +32,34 @@ class Listing(models.Model):
         (2, 'Villa/Radhus')
     )
     
-    BRF_STATUS_CHOICES = ((0, 'Ej bildad'), (1, 'Blivande'), (2, 'Bildad'))
+    BRF_STATUS_CHOICES = ((0, 'Ej bildad'), (1, 'Förening Bildad'), (2, 'Blivande Bostadsrätt'))
+    BRF_WANTED_STATUS_CHOICES = ((0, 'Nej'), (1, 'Förening Bildad eller Blivande Bostadsrätt'), (2, 'Blivande Bostadsrätt'))
     
     MAX_ROOMS_CHOICE = 6
+    rooms_choices = generate_rooms_choices(MAX_ROOMS_CHOICE)
     
     creation_datetime = models.DateTimeField(auto_now_add=True)
+    o_county = SECountySelect()
     o_street_address = models.CharField(max_length=255)
-    o_postal_code = models.CharField(max_length=255)
-    o_rooms = models.PositiveIntegerField(choices = generate_rooms_choices(MAX_ROOMS_CHOICE)) # The number of rooms times 2, to avoid having to use floats or decimals
+    o_postal_code = SEPostalCodeField
+    o_rooms = models.PositiveIntegerField(choices = rooms_choices) # The number of rooms times 2, to avoid having to use floats or decimals
     o_area = models.PositiveIntegerField() # area in square meters
     o_type = models.PositiveIntegerField(choices=HOUSING_TYPE_CHOICES)
+    o_rent = models.PositiveIntegerField()
     o_description = models.TextField()
-    o_brf_status = models.PositiveIntegerField(choices = BRF_STATUS_CHOICES)
+    o_brf_status = models.PositiveIntegerField(choices=BRF_STATUS_CHOICES)
+    o_floor_no = models.IntegerField()
+    o_has_fireplace = models.BooleanField()
+    o_has_balcony = models.BooleanField()
+    o_has_elevator = models.BooleanField()
+    
+    w_county = SECountySelect()
+    w_min_rooms = models.PositiveIntegerField(choices = rooms_choices)
+    w_min_area = models.PositiveIntegerField() # area in square meters
+    w_max_rent = models.PositiveIntegerField()
+    w_types = models.PositiveIntegerField(choices=HOUSING_TYPE_CHOICES)
+    w_brf_status = models.PositiveIntegerField(choices=BRF_WANTED_STATUS_CHOICES)
+    w_has_fireplace = models.NullBooleanField()
+    w_has_balcony = models.NullBooleanField()
+    w_has_elevator = models.NullBooleanField()
+    w_not_bottom_floor = models.NullBooleanField()
