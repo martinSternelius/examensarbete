6e416a8cd7f491da468086bb36c409362b8669bd
martinSternelius <martin.sternelius@gmail.com>
2013-10-01 22:19:29 +0200

    Replaces the Listing form test with a more useful and thorough one that tests the create_listing view instead

--- housingtrader/tests.py
+++ housingtrader/tests.py
@@ -7,30 +7,81 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test.client import Client
 from django.contrib.auth.models import User
-from housingtrader.models import Listing, TYPE_BRF, TYPE_TENANCY, TYPE_HOUSE, BRF_NONE,\
-    BRF_FORMED
+from housingtrader.models import Listing, TYPE_BRF, TYPE_TENANCY, TYPE_HOUSE, BRF_NONE, BRF_FORMED
 from housingtrader.forms import CompleteListingForm
 from django.http.request import QueryDict
 
-class ListingFormTests(TestCase):
+class ListingViewTests(TestCase):
     def setUp(self):
         self.user = User.objects.create_user('tester', 'test@example.com', 'password')
         
     def tearDown(self):
         self.user.delete()
         
-    def test_save(self):
+    def test_create_listing_view(self):
         '''
-        Assertion: The form correctly processes the w_housing_types fields and saves the list of number as a string of comma separated numbers
+        Assertion: A logged in user that sends valid POST data to create_listing will result in a listing correctly saved in the database.
         '''
-        query_string = 'w_min_rooms=8&w_has_elevator=2&o_rent=6000&o_postal_code=14414&o_has_balcony=on&o_area=50&w_county=AB&o_brf_status=0&w_has_fireplace=2&o_floor_no=5&w_types=1&w_types=2&w_max_rent=10000&o_street_address=Testgatan+1&o_type=1&w_has_balcony=2&o_rooms=5&o_has_elevator=on&o_description=En+testl%C3%A4genhet&w_not_bottom_floor=1&w_min_area=60&o_county=AB&w_brf_status=1'
-        form = CompleteListingForm(data=QueryDict(query_string), user=self.user)
+        client = Client()
+        client.login(username='tester', password='password')
         
-        form.save()
+        asserted_listing = Listing()
+        asserted_listing.o_area = 33
+        asserted_listing.o_brf_status = None
+        asserted_listing.o_county = 'AB'
+        asserted_listing.o_description = 'Min'
+        asserted_listing.o_floor_no = -1
+        asserted_listing.o_has_balcony = 1
+        asserted_listing.o_has_elevator = 0
+        asserted_listing.o_has_fireplace = 0
+        asserted_listing.o_postal_code = '16846'
+        asserted_listing.o_rent = 1722
+        asserted_listing.o_rooms = 2
+        asserted_listing.o_street_address = 'Klientvägen 1'
+        asserted_listing.o_type = TYPE_BRF
+        asserted_listing.w_brf_status = None
+        asserted_listing.w_county = 'AB'
+        asserted_listing.w_has_balcony = False
+        asserted_listing.w_has_elevator = False
+        asserted_listing.w_has_fireplace = False
+        asserted_listing.w_max_rent = 5000
+        asserted_listing.w_min_area = 33
+        asserted_listing.w_min_rooms = 2
+        asserted_listing.w_not_bottom_floor = False
+        asserted_listing.w_types = ','.join([str(TYPE_TENANCY), str(TYPE_BRF)])
         
-        listing = Listing.objects.get(o_description='En testlägenhet')
-        self.assertEqual(listing.w_types, '1,2')
+        data = {
+            'o_area' : 33,
+            'o_brf_status' : '',
+            'o_county' : 'AB',
+            'o_description' : 'Min',
+            'o_floor_no' : -1,
+            'o_has_balcony' : 1,
+            'o_has_elevator' : 0,
+            'o_has_fireplace' : 0,
+            'o_postal_code' : '16846',
+            'o_rent' : 1722,
+            'o_rooms' : 2,
+            'o_street_address' : 'Klientvägen 1',
+            'o_type' : TYPE_BRF,
+            'w_brf_status' : '',
+            'w_county' : 'AB',
+            'w_has_balcony' : 0,
+            'w_has_elevator' : 0,
+            'w_has_fireplace' : 0,
+            'w_max_rent' : 5000,
+            'w_min_area' : 33,
+            'w_min_rooms' : 2,
+            'w_not_bottom_floor' : 0,
+            'w_types' : [TYPE_TENANCY, TYPE_BRF]
+        }
+        client.post('/create_listing/', data)
+        
+        listing = Listing.objects.get(o_description='Min')
+        listing.delete() # Sets pk to none, but keeps all other data, which should make it identical to the asserted listing
+        self.assertEqual(asserted_listing, listing)
         
 class ListingModelTests(TestCase):
     def setUp(self):
@@ -80,7 +131,7 @@ class ListingModelTests(TestCase):
         dummy_listing.o_postal_code = '16846'
         dummy_listing.o_rent = 1722
         dummy_listing.o_rooms = 2
-        dummy_listing.o_street_address = 'Arvid Mörnes väg 42'
+        dummy_listing.o_street_address = 'Annan adress'
         dummy_listing.o_type = TYPE_BRF
         dummy_listing.w_brf_status = None
         dummy_listing.w_county = 'AB'
@@ -94,7 +145,6 @@ class ListingModelTests(TestCase):
         dummy_listing.w_types = ','.join([str(TYPE_TENANCY), str(TYPE_BRF)])
         
         dummy_listing.user = self.other_user
-        dummy_listing.o_street_address = 'Annan adress'
         dummy_listing.save()
         self.dummy_listing = dummy_listing
         
