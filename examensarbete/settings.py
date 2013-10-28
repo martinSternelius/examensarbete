24fc905f4547554f3a13d20098be606f1e7cdb23
martinSternelius <martin.sternelius@gmail.com>
2013-10-28 12:13:54 +0100

    Moves common static files into the project path
    They are no longer in a separate repo. Thank god! Ignore that line
    in the last commit.

--- examensarbete/settings.py
+++ examensarbete/settings.py
@@ -1,5 +1,8 @@
 # Django settings for examensarbete project.
+import os
 import localsettings
+
+PROJECT_DIR = os.path.dirname(__file__)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -52,14 +55,19 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static_collected')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = localsettings.STATICFILES_DIRS
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'static_common'),
+)
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -94,7 +102,12 @@ ROOT_URLCONF = 'examensarbete.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'examensarbete.wsgi.application'
 
-TEMPLATE_DIRS = localsettings.TEMPLATE_DIRS
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'templates')
+)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
